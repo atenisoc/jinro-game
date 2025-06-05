07c7b6f5d9d4491aa5eb859ba6a5241209002137
atenisoc <at_enisoc@yahoo.co.jp>
2025-06-05 17:59:59 +0900

    Temp: remove NpcCard to isolate build error

--- src/components/NpcList.tsx
+++ src/components/NpcList.tsx
@@ -1,22 +1,8 @@
-import NpcCard, { Props } from './NpcCard';
-
-const npcData: Props[] = [
-  {
-    name: 'タツマキ',
-    image: '/images/tatsumaki.png',
-    role: '霊能者',
-    message: '私は超能力で真実を見抜くわよ。',
-  },
-  {
-    name: '灰原哀',
-    image: '/images/haibara.png',
-    role: '占い師',
-    message: '理論的に見て、あなたが怪しいわね。',
-  },
-  {
-    name: 'ルルーシュ',
-    image: '/images/lulu.png',
-    role: '村人',
-    message: '……この状況、計算通りだ。',
-  },
-];
+// src/components/NpcList.tsx
+export default function NpcList() {
+  return (
+    <div>
+      <p>NPC一覧（仮）</p>
+    </div>
+  );
+}
