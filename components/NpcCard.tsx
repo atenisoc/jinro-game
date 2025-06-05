36e55b4929ea3386f522c9605d99c9fba5b9a3a4
atenisoc <at_enisoc@yahoo.co.jp>
2025-06-05 09:49:22 +0900

    Fix: replace JSX.Element with React.FC in NpcCard

--- components/NpcCard.tsx
+++ components/NpcCard.tsx
@@ -1,13 +1,16 @@
 // components/NpcCard.tsx
+import React from 'react';
 
 type Props = {
   name: string;
 };
 
-export default function NpcCard({ name }: Props): JSX.Element {
+const NpcCard: React.FC<Props> = ({ name }) => {
   return (
     <div className="border rounded p-2 shadow">
       <p className="text-lg font-semibold">{name}</p>
     </div>
   );
-}
+};
+
+export default NpcCard;
