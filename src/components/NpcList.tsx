5dacb9ef2e91b523aedb88871b00714a09c77f5a
atenisoc <at_enisoc@yahoo.co.jp>
2025-06-05 18:26:09 +0900

    Temp: simplify NpcList to remove NpcCard dependency

--- src/components/NpcList.tsx
+++ src/components/NpcList.tsx
@@ -1,8 +1,7 @@
 export default function NpcList() {
   return (
-    <div className="flex flex-col gap-4">
-      <h2 className="text-xl font-bold mb-2">NPC一覧</h2>
-      <p>（表示テスト中）</p>
+    <div>
+      <h2>テスト表示：NpcList 単体</h2>
     </div>
   );
 }
