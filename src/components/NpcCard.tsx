00d44bf3bbb4d007f56470d4ed0633d61bf8901a
atenisoc <at_enisoc@yahoo.co.jp>
2025-06-05 10:14:19 +0900

    Fix: move NpcCard.tsx to src/components

---
+++ src/components/NpcCard.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  name: string;
+};
+
+export default function NpcCard({ name }: Props) {
+  return (
+    <div className="border rounded p-2 shadow">
+      <p className="text-lg font-semibold">{name}</p>
+    </div>
+  );
+}
